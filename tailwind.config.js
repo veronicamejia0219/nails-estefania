39c806d5883d1bfce4cd813086e01c472e547c68
Veronica Mejia <veronica.mejiam@autonoma.edu.co>
2026-08-14 14:14:04 -0500

    feat: configuracion manual de tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "rosa-suave": "#FCE7F3",
+        "dorado-lujo": "#D4AF37",
+      },
+    },
+  },
+  plugins: [],
+};
